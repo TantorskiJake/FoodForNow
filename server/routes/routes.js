138eec4886122fc2c6317bf7f2d55ceca2ef7ec8
Jake Tantorski <jake.tantorski1@marist.edu>
2024-01-17 17:50:01 -0500

    added comments to route.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,14 @@
+// Import the 'express' module to create a web server.
 const express = require('express');
-const routes = express.Router()
-routes.get('/',(req,res) => {res.send('Hello world or soemthin')})
-module.exports = {routes}
+
+// Create an instance of the Express Router.
+const routes = express.Router();
+
+// Define a route for the root path ('/') using the 'get' method.
+routes.get('/', (req, res) => {
+  // When a request is made to the root path, send the response 'Hello world or something'.
+  res.send('Hello world or something');
+});
+
+// Export the 'routes' object to make it accessible to other parts of the application.
+module.exports = { routes };
