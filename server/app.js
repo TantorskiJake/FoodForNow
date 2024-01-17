e430cb4f0b599a1288d0aa23e41baa30a05ef3a4
Jake Tantorski <jake.tantorski1@marist.edu>
2024-01-17 17:25:09 -0500

    added mongo connection

--- server/app.js
+++ server/app.js
@@ -1,29 +1,37 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 8080;
 
 // MongoDB connection URL and database name
-const url = 'mongodb+srv://JakeTantorski:JakeTantorski@foodfornowrecipes.i9zgp80.mongodb.net/?retryWrites=true&w=majority';
+const uri = "mongodb+srv://JakeTantorski:JakeTantorski@foodfornowrecipes.i9zgp80.mongodb.net/?retryWrites=true&w=majority";
 const dbName = 'FoodForNowRecipes';
 
-// Update your MongoClient connection code
-MongoClient.connect(url, (err, client) => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
-    }
 
-    console.log(`Connected to MongoDB database: ${dbName}`);
-
-    // Get the database instance
-    const db = client.db(dbName);
-
-    // Additional setup or routes can go here
-
-   
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
  // Start the Express server
  app.listen(port, () => {
