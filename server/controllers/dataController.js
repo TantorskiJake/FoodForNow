433e56f0ba91dbe4a4fd2ea3fa96fefa9ecae102
Jake Tantorski <jake.tantorski1@marist.edu>
2024-01-29 16:22:25 -0500

    wrote comments

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,30 +1,52 @@
 // controllers/dataController.js
 
+// Import necessary MongoDB library components
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+// MongoDB connection URI and database name
 const uri = "mongodb+srv://JakeTantorski:JakeTantorski@foodfornowrecipes.i9zgp80.mongodb.net/?retryWrites=true&w=majority";
 const dbName = 'FoodForNow';
 
+/**
+ * Establish a connection to MongoDB and return the connected database instance.
+ * @returns {Promise} A Promise that resolves to the MongoDB database instance.
+ */
 const connectToMongoDB = async () => {
+  // Create a new MongoClient instance with the specified URI and server options
   const client = new MongoClient(uri, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+      version: ServerApiVersion.v1,  // Specify the MongoDB Server API version
+      strict: true,                   // Enable strict mode for API usage
+      deprecationErrors: true,        // Report deprecation errors
     },
   });
 
+  // Connect to the MongoDB server
   await client.connect();
+
+  // Return the connected database instance
   return client.db(dbName);
 };
 
+/**
+ * Retrieve recipes from the MongoDB database.
+ * @returns {Promise} A Promise that resolves to an array of recipe documents.
+ */
 const getRecipes = async () => {
+  // Connect to MongoDB
   const db = await connectToMongoDB();
+
+  // Access the 'Recipes' collection
   const collection = db.collection('Recipes');
+
+  // Fetch documents with specified projection (include only _id, title, and ingredients fields)
   const documents = await collection.find({}, { projection: { _id: 1, title: 1, ingredients: 1 } }).toArray();
+
+  // Return the fetched documents
   return documents;
 };
 
+// Export the getRecipes function to make it accessible to other modules
 module.exports = {
   getRecipes,
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,19 +1,26 @@
 // routes.js
 
+// Import the express library and the getRecipes function from the dataController module
 const express = require('express');
 const { getRecipes } = require('../controllers/dataController');
 
+// Create an instance of an Express router
 const router = express.Router();
 
+// Define a route to handle HTTP GET requests at the root endpoint ('/')
 router.get('/', async (req, res) => {
   try {
-    // Use the getRecipes function from the dataController to fetch data from MongoDB
+    // Attempt to fetch recipes by calling the getRecipes function from the dataController
     const documents = await getRecipes();
+
+    // Respond with a JSON representation of the fetched recipes
     res.json(documents);
   } catch (error) {
+    // If an error occurs during the fetch operation, log the error and send a 500 Internal Server Error response
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
+// Export the router instance to make it accessible to other modules
 module.exports = { routes: router };
