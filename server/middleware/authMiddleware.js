b9efcdc8fbea331852985276ecd7ec11d092222e
Jake Tantorski <jake.tantorski1@marist.edu>
2024-02-21 00:32:53 -0500

    reset example branch

--- server/middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-  
-  module.exports = isAuthenticated;
-  
