7e60d011d37e7eaf5f1b658fed6a91f4f7bd0e71
Jake Tantorski <jake.tantorski1@marist.edu>
2024-08-17 00:19:15 -0400

    added !

--- GPTAutoUpload/upload_recipe.py
+++ GPTAutoUpload/upload_recipe.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import json
 
 def connect_to_mongo(uri, db_name):
-    """Connect to MongoDB."""
+    """Connect to MongoDB!"""
     client = MongoClient(uri)
     return client[db_name]
 
@@ -25,29 +25,16 @@ def read_recipe_from_string(recipe_string):
 
 def main():
     # MongoDB connection parameters
-    uri = "mongodb+srv://JakeTantorski:JakeTantorski@foodfornowrecipes.i9zgp80.mongodb.net/"  # Replace with your MongoDB URI
+    uri = "mongodb+srv://JakeTantorski:JakeTantorski@foodfornowrecipes.i9zgp80.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE" # Replace with your MongoDB URI
     db_name = "recipes_db"
     collection_name = "recipes"
 
     # Connect to MongoDB
     db = connect_to_mongo(uri, db_name)
 
-    # Recipe data as JSON string or file path
-    # You can use either the JSON string method or the file method.
-    # Example JSON string (You can also replace this with a file path to read from a file):
-    # recipe_string = '''
-    #      {
-    #     "name": "Braised Beef Short Ribs with Risotto Milanese",
-    #     "ingredients": [ ... ],
-    #     "instructions": [ ... ]
-    # }
-
-    # '''
-
     # Read recipe data (choose one method)
     recipe = read_recipe_from_file('recipe.json')  # Uncomment this line if using a file
-    # recipe = read_recipe_from_string(recipe_string)  # Uncomment this line if using a JSON string
-
+    
     # Insert the recipe into MongoDB
     inserted_id = insert_recipe(db, collection_name, recipe)
     print(f"Recipe inserted with ID: {inserted_id}")
