a1fcfe98f59420bd5bcb7773732c239a66cb490e
JWilson45 <jason.wilson2@marist.edu>
2025-06-16 22:08:09 -0400

    remove unused

--- foodfornow-frontend/src/pages/Ingredients.jsx
+++ foodfornow-frontend/src/pages/Ingredients.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Container,
-  Grid,
-  Card,
-  CardContent,
   Typography,
   Button,
   Box,
