d2bbd3effe6218bac6e7c89856658319d10269a7
Jason Wilson <35709473+JWilson45@users.noreply.github.com>
2025-06-17 10:10:03 -0400

    feat(frontend): disable StrictMode in prod

--- foodfornow-frontend/src/main.jsx
+++ foodfornow-frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+const element = import.meta.env.DEV ? (
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+) : (
+  <App />
+);
+
+root.render(element);
