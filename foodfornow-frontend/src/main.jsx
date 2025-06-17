3b507d6b90c965616c167301d8e42f3beba2ce11
Jason Wilson <35709473+JWilson45@users.noreply.github.com>
2025-06-17 10:16:37 -0400

    docs: add build instructions

--- foodfornow-frontend/src/main.jsx
+++ foodfornow-frontend/src/main.jsx
@@ -4,7 +4,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  import.meta.env.DEV ? (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  ) : (
     <App />
-  </StrictMode>,
+  ),
 )
